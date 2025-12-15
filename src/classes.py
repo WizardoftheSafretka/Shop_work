7cf4bc46de3f2020dd42faf02dce9ba239988ed9
Wizard <merihov2yandex.ru>
2025-12-15 20:58:57 +0300

    update classes.py: add getters, setters, new methods

--- src/classes.py
+++ src/classes.py
@@ -9,8 +9,32 @@ class Product:
 
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, new_object: dict):
+        """Метод, которыйпринимать на вход параметры товара в словаре и возвращать созданный объект класса"""
+
+        name, description, price, quantity = new_object.values()
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """Геттер возвращает значение приватного атрибута цены"""
+
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        """Сеттер устанавливает новое значение приватного атрибута цены"""
+        if new_price > 0:
+           self.__price == new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
+
+
+
 
 
 class Category:
@@ -25,6 +49,28 @@ class Category:
 
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product):
+            """Для добавления товаров в категорию"""
+
+            self.products.append(product)
+            Category.product_count += 1
+
+    @property
+    def products(self):
+        """Геттер, который будет выводить список товаров в виде строк"""
+
+        product = ""
+        for product in self.__products:
+            product += f"{Product.name}, {Product.price} руб. Остаток: {Product.quantity} шт.\n"
+        return product
+
+
+
+
+
+
+
