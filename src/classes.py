aaaf107158cb564c16aee78b13ecc442f7bdc30e
Wizard <merihov2yandex.ru>
2025-12-16 18:54:13 +0300

    update and test classes.py

--- src/classes.py
+++ src/classes.py
@@ -14,7 +14,7 @@ class Product:
 
     @classmethod
     def new_product(cls, new_object: dict):
-        """Метод, которыйпринимать на вход параметры товара в словаре и возвращать созданный объект класса"""
+        """Метод, который должен принимать на вход параметры товара в словаре и возвращать созданный объект класса"""
 
         name, description, price, quantity = new_object.values()
         return cls(name, description, price, quantity)
@@ -29,7 +29,7 @@ class Product:
     def price(self, new_price: float):
         """Сеттер устанавливает новое значение приватного атрибута цены"""
         if new_price > 0:
-           self.__price == new_price
+           self.__price = new_price
         else:
             print("Цена не должна быть нулевая или отрицательная")
 
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,10 +1,12 @@
-from src.classes import Category
+from pyexpat.errors import messages
+
+from src.classes import Category, Product
 
 
 def test_category_init(category):
     assert category.name == "Овощи"
     assert category.description == "Обыкновенные овощи"
-    assert len(category.products) == 2
+    assert len(category._Category__products) == 2
     assert Category.category_count == 1
     assert Category.product_count == 2
 
@@ -14,3 +16,28 @@ def test_product_init(product):
     assert product.description == "Обыкновенные огурцы"
     assert product.price == 100.0
     assert product.quantity == 1
+
+
+
+def test_new_product():
+    new_object = {"name": "Вещь", "description": "Просто вещь", "price": 100500.0, "quantity": 1}
+    result = Product.new_product(new_object=new_object)
+    assert result.name == "Вещь"
+    assert result.description == "Просто вещь"
+    assert result.price == 100500.0
+    assert result.quantity == 1
+
+def test_price(capsys, product):
+    assert product.price == 100.0
+    product.price = 1
+    assert product.price == 1
+    product.price = -1
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
+
+def test_add_product(category, product):
+    category.add_product(product)
+    assert category.product_count == 5
+
+def test_products(category):
+    assert category.products == "Огурцы, 100.0 руб. Остаток: 1 шт.\nПомироды, 120.5 руб. Остаток: 2 шт.\n"
