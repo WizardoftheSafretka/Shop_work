623599d9d091b45eb77e5ee7f587c2b6a1ab736e
Wizard <merihov2yandex.ru>
2025-12-17 19:46:24 +0300

    add magic methods

--- src/classes.py
+++ src/classes.py
@@ -12,6 +12,12 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        return self.__price * self.quantity + other.__price * other.quantity
+
     @classmethod
     def new_product(cls, new_object: dict):
         """Метод, который должен принимать на вход параметры товара в словаре и возвращать созданный объект класса"""
@@ -53,6 +59,12 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products)
 
+    def __str__(self):
+        count = 0
+        for product in self.__products:
+            count += int(product.quantity)
+        return f"{self.name}, количество продуктов: {count} шт."
+
     def add_product(self, product: Product):
             """Для добавления товаров в категорию"""
 
@@ -67,8 +79,6 @@ class Category:
         for product in self.__products:
             str_product += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
         return str_product
-
-
 
 
 
